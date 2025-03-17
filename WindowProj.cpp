4d96b3b2c5393746e1a520ae7f75fe6e3c45da37
square7024 <82199971+square7024@users.noreply.github.com>
2025-03-17 13:17:00 +0900

    윈프
    윈도우 프로그래밍

---
+++ WindowProj.cpp
@@ -0,0 +1,61 @@
+#include <windows.h>
+#include <tchar.h>
+
+HINSTANCE g_hInst;
+LPCTSTR IpszClass = L"Window Class Name";
+LPCTSTR	IpszWindowName = L"Window Programming Lab";
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInstance, LPSTR IpszCmdParam, int nCmdShow)
+{
+	HWND hWnd;
+	MSG message;
+	WNDCLASSEX WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbSize = sizeof(WndClass);
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	WndClass.lpfnWndProc = (WNDPROC)WndProc;
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hInstance = hInstance;
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	WndClass.lpszMenuName = NULL;
+	WndClass.lpszClassName = IpszClass;
+	WndClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	RegisterClassEx(&WndClass);
+
+	hWnd = CreateWindow(IpszClass, IpszWindowName, WS_OVERLAPPEDWINDOW, 0, 0, 800, 600, NULL, (HMENU)NULL, hInstance, NULL);
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	while (GetMessage(&Message, 0, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lPARAM)
+{
+	PAINTSTRUCT ps;
+	HDC hDC;
+	TCHAR temp[] = TEXT("Hello world!");
+	int x = 0, y = 0;
+
+	switch (iMessage) {
+	case WM_PAINT:
+		hDC = BeginPaint(hWnd, &ps);
+		TextOut(hDC, x, y, temp, lstrlen(temp));
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return (DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
